e532b0f0fecc4d88dda77e3ab86e0c8510117b3c
Jeziel Marques <jezielmarques7@gmail.com>
2020-04-01 03:25:26 -0300

    small implementations

--- functs/string_funcs/js/string_funcs.js
+++ functs/string_funcs/js/string_funcs.js
@@ -1,9 +1,12 @@
 function string() {
   const name = 'Jeziel marques';
 
-  document.write(name.length + '<hr/>');
-  document.write(name[7] + ' | ');
-  document.write(name.indexOf('z'));
+  document.write(name + ' = ' + name.length + ' strings.');
+  document.write('<hr/>');
+  
+  document.write(name[0].toUpperCase() + name[7]);
+  document.write(' | ');
+  document.write(name.indexOf('m'));
 };
 
 string(name);
