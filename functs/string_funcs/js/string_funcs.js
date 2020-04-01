297721ecc628cab5822a60ce30202fd109b5e835
Jeziel Marques <jezielmarques7@gmail.com>
2020-04-01 04:24:08 -0300

    small implementations

--- functs/string_funcs/js/string_funcs.js
+++ functs/string_funcs/js/string_funcs.js
@@ -1,12 +1,31 @@
 function string() {
-  const name = 'Jeziel marques';
+  const name = 'Jeziel Marques Braga';
 
-  document.write(name + ' = ' + name.length + ' strings.');
-  document.write('<hr/>');
+  const newName = name.replace('Braga', 'B.');
+
+  document.write(newName + ' = ' + (name.length - 3) + ' strings.');
+  document.write('<br/> --------- <br/>');
   
-  document.write(name[0].toUpperCase() + name[7]);
+  document.write(name[0].toUpperCase() + name[7].toLowerCase());
   document.write(' | ');
-  document.write(name.indexOf('m'));
+  document.write(name.indexOf('M'));
+  document.write('<br/> -------- -------- -------- --- <br/>');
+
+  document.write('<b>Your name:</b> ');
+  document.write(name.substr(0, 6));
+
+  document.write('<br/>');
+
+  document.write('<b>Middle name:</b> ');
+  document.write(name.substr(7, 7));
+
+  document.write('<br/>');
+
+  document.write('<b>Last name:</b> ');
+  document.write(name.substr(15, 5));
+
+  document.write('<br/> -------- -------- -------- --- <br/>');
+
 };
 
 string(name);
