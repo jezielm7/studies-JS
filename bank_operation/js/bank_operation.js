d6a434be93fabbe2078b14427ed24a2be95c2873
Jeziel Marques <jezielmarques7@gmail.com>
2020-04-02 13:13:12 -0300

    updated conditional structure

--- bank_operation/js/bank_operation.js
+++ bank_operation/js/bank_operation.js
@@ -1,10 +1,17 @@
-let saldo = 82;
-let transf = 70;
+let saldo = 70;
+let transf = 72;
 
 function condicion(saldo, transf) {
 
   if(saldo < transf || !saldo) {
+
+    if(saldo === transf - 2) {
+      document.write('Operação concluída com sucesso!');
+      document.write('Saldo faltante debidatado da conta e será descontado no próximo depósito.');
+    } else {
     document.write('Não há saldo suficiente para concluir a transferência!');
+    }
+
   } else {
     document.write('Operação concluída com sucesso!');
   }
